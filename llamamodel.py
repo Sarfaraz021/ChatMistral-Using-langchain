34c1b12edbf4599da3f8f45830c41813c75022fc
Sarfaraz Ahmed <20cs067@students.muet.edu.pk>
2024-03-20 02:07:56 +0500

    Update llamamodel.py

--- llamamodel.py
+++ llamamodel.py
@@ -5,14 +5,12 @@ from langchain.llms import LlamaCpp
 from langchain.prompts import PromptTemplate
 
 
-
-
-
 # Callbacks support token-wise streaming
 
 def load_model() -> LlamaCpp:
-    
-    callback_manager: CallbackManager = CallbackManager([StreamingStdOutCallbackHandler()])
+
+    callback_manager: CallbackManager = CallbackManager(
+        [StreamingStdOutCallbackHandler()])
     Model_path = "C:\LLAMA2Locally\model\llama-2-13b-chat.Q3_K_M.gguf"
     # Make sure the model path is correct for your system!
     Llama_model: LlamaCpp = LlamaCpp(
@@ -21,7 +19,7 @@ def load_model() -> LlamaCpp:
         max_tokens=2000,
         top_p=1,
         callback_manager=callback_manager,
-        verbose=True # Verbose is required to pass to the callback manager
+        verbose=True  # Verbose is required to pass to the callback manager
     )
     return Llama_model
 
